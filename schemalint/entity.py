ab88fd03db8652b5498b883df7a17f985995fc7c
podhmo <ababjam61+github@gmail.com>
2019-09-21 21:46:04 +0900

    refactor: more entity

--- schemalint/entity.py
+++ schemalint/entity.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 import typing as t
+import logging
 from dataclasses import dataclass, field
 
 from typing_extensions import Protocol
 
 from .errors import Error
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
@@ -23,3 +26,18 @@ class Context:
 class Lookup(Protocol):
     def lookup_node(self, data: object) -> object:
         ...
+
+
+class NodeStore(Lookup):
+    def __init__(self):
+        self.cache = {}
+
+    def add_node(self, name, node, r):
+        logger.debug("add_node %s", name)
+        if r is None:
+            return r
+        self.cache[id(r)] = node
+        return r
+
+    def lookup_node(self, data: object) -> object:
+        return self.cache[id(data)]
