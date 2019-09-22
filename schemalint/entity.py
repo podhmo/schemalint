60fc26d5b19441489d7d882497190d833443e94c
podhmo <ababjam61+github@gmail.com>
2019-09-23 00:07:37 +0900

    feat: logger adapter

--- schemalint/entity.py
+++ schemalint/entity.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 import typing as t
+import types
 import logging
 from dataclasses import dataclass, field
 
 from typing_extensions import Protocol
+from dictknife.langhelpers import reify
 
 from .errors import Error
 
@@ -41,3 +43,79 @@ class NodeStore(Lookup):
 
     def lookup_node(self, data: object) -> object:
         return self.cache[id(data)]
+
+
+_SysExcInfoType = t.Union[
+    t.Tuple[type, BaseException, t.Optional[types.TracebackType]],
+    t.Tuple[None, None, None],
+]
+_ExcInfoType = t.Union[None, bool, _SysExcInfoType, BaseException]
+
+
+class Logger(Protocol):
+    def debug(
+        self,
+        msg: t.Any,
+        *args: t.Any,
+        exc_info: _ExcInfoType = ...,
+        stack_info: bool = ...,
+        extra: t.Optional[t.Dict[str, t.Any]] = ...,
+        **kwargs: t.Any
+    ) -> None:
+        ...
+
+    def info(
+        self,
+        msg: t.Any,
+        *args: t.Any,
+        exc_info: _ExcInfoType = ...,
+        stack_info: bool = ...,
+        extra: t.Optional[t.Dict[str, t.Any]] = ...,
+        **kwargs: t.Any
+    ) -> None:
+        ...
+
+    def warning(
+        self,
+        msg: t.Any,
+        *args: t.Any,
+        exc_info: _ExcInfoType = ...,
+        stack_info: bool = ...,
+        extra: t.Optional[t.Dict[str, t.Any]] = ...,
+        **kwargs: t.Any
+    ) -> None:
+        ...
+
+    def warn(
+        self,
+        msg: t.Any,
+        *args: t.Any,
+        exc_info: _ExcInfoType = ...,
+        stack_info: bool = ...,
+        extra: t.Optional[t.Dict[str, t.Any]] = ...,
+        **kwargs: t.Any
+    ) -> None:
+        ...
+
+    def error(
+        self,
+        msg: t.Any,
+        *args: t.Any,
+        exc_info: _ExcInfoType = ...,
+        stack_info: bool = ...,
+        extra: t.Optional[t.Dict[str, t.Any]] = ...,
+        **kwargs: t.Any
+    ) -> None:
+        ...
+
+
+class LoggerWithCollectMessage(logging.LoggerAdapter, Logger):
+    @reify
+    def history(self) -> t.List[str]:
+        return []
+
+    def process(
+        self, msg: t.Any, kwargs: t.MutableMapping[str, t.Any]
+    ) -> t.Tuple[t.Any, t.MutableMapping[str, t.Any]]:
+        self.history.append(msg)
+        return (msg, kwargs)
